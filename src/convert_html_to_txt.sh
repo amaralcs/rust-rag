7a9378242ef4af0cad53ff90be9a8603c616a109
amaralcs <amaralcscgp@gmail.com>
2024-11-10 15:54:22 +0000

    Filter only relevant chapters

--- src/convert_html_to_txt.sh
+++ src/convert_html_to_txt.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 BOOK_DIR="./book/"
-PDF_DIR="./pdf/"
+TXT_DIR="./txt/"
 
-if [ ! -d "$PDF_DIR" ]; then
-    mkdir "$PDF_DIR"
+if [ ! -d "$TXT_DIR" ]; then
+    mkdir "$TXT_DIR"
 fi
 
-files=$(find "$BOOK_DIR" -type f -name "*.html")
+files=$(find "$BOOK_DIR" -type f -name "*.html" | grep -E "ch[0-9]{2}")
 
 count=0
 for file in $files; do
+    filename=$(basename "$file" .html)
+    
+    # Remove the sidebar content based on its HTML structure
+    sed '/<nav id="sidebar" class="sidebar" aria-label="Table of contents">/,/<\/nav>/d' "$file" | \
+        sed '/<div id="menu-bar" class="menu-bar sticky">/,/<\/div>/d' | \
+        pandoc --no-highlight --from html --to plain --output "$TXT_DIR$filename".txt -
+        
     if ((count % 50 == 0)); then
         echo "Processed $count ..."
     fi
-
     count=$((count + 1))
-    filename=$(basename "$file" .html)
-    
-
-    # pandoc -s -o "$PDF_DIR$filename".pdf "$file"
-    pandoc --from html --to plain --output "$PDF_DIR$filename".txt "$file"
 
 done
